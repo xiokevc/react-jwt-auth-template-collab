cd93be5f3664e45158e28e4b9ed85d5a6623bf70
jelesho <jelesho100@gmail.com>
2025-08-18 16:45:33 -0400

    useContext Hook

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,36 @@
+// src/components/NavBar/NavBar.jsx
+
+// Import the useContext hook
+import { useContext } from 'react';
+import { Link } from 'react-router';
+
+// Import the UserContext object
+import { UserContext } from '../../contexts/UserContext';
+
 const NavBar = () => {
+  // Pass the UserContext object to the useContext hook to access:
+  // - The user state (which we use here).
+  // - The setUser function to update the user state (which we aren't using).
+  //
+  // Destructure the object returned by the useContext hook for easy access
+  // to the data we added to the context with familiar names.
+  const { user } = useContext(UserContext);
+
   return (
     <nav>
-      <p>Navbar contents will go here.</p>
+      {user ? (
+        <ul>
+          <li>Welcome, {user.username}</li>
+        </ul>
+      ) : (
+        <ul>
+          <li><Link to='/sign-up'>Sign Up</Link></li>
+        </ul>
+      )}
     </nav>
   );
 };
 
 export default NavBar;
+
 
