914c6a3005e0c65d299053f57a1db90e7ac7e8df
smdhar <shoumik_dhar@yahoo.com>
2025-08-18 10:55:23 -0400

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import NavBar from "./components/NavBar/NavBar";
+
+const App = () => {
+  
+  return (
+    <>
+      <NavBar />
+      <h1>Hello, friend!</h1>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,10 @@
+const NavBar = () => {
+  return (
+    <nav>
+      <p>Navbar contents will go here.</p>
+    </nav>
+  );
+};
+
+export default NavBar;
+
