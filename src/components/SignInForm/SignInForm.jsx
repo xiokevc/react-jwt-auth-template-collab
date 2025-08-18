afd7cff9dceb163c014e7a4caa24ceae6745b29a
xiokevc <xiokevc@gmail.com>
2025-08-18 16:14:44 -0400

    add SignInForm feature

---
+++ src/components/SignInForm/SignInForm.jsx
@@ -0,0 +1,69 @@
+import { useState, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { signIn } from '../../services/authService';
+import { UserContext } from '../../contexts/UserContext';
+
+const SignInForm = () => {
+  const navigate = useNavigate();
+  const { setUser } = useContext(UserContext);
+  const [message, setMessage] = useState('');
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  });
+
+  const handleChange = (evt) => {
+    setMessage('');
+    setFormData({ ...formData, [evt.target.name]: evt.target.value });
+  };
+
+  const handleSubmit = async (evt) => {
+    evt.preventDefault();
+    try {
+      const signedInUser = await signIn(formData);
+      setUser(signedInUser);
+      navigate('/');
+    } catch (err) {
+      setMessage(err.message);
+    }
+  };
+
+  return (
+    <main>
+      <h1>Sign In</h1>
+      <p>{message}</p>
+      <form autoComplete='off' onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor='username'>Username:</label>
+          <input
+            type='text'
+            id='username'
+            name='username'
+            value={formData.username}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor='password'>Password:</label>
+          <input
+            type='password'
+            id='password'
+            name='password'
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <button type='submit'>Sign In</button>
+          <button type='button' onClick={() => navigate('/')}>
+            Cancel
+          </button>
+        </div>
+      </form>
+    </main>
+  );
+};
+
+export default SignInForm;
