8177ca6b3bb7d4f7eeaafebfca58179f19302c90
jelesho <jelesho100@gmail.com>
2025-08-18 16:52:44 -0400

    updated auto sign in

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -4,21 +4,25 @@ import { createContext, useState } from 'react';
 
 const UserContext = createContext();
 
+const getUserFromToken = () => {
+    const token = localStorage.getItem('token');
+
+    if (!token) return null;
+
+    return JSON.parse(atob(token.split('.')[1])).payload;
+};
+
 function UserProvider({ children }) {
-  // Create state just like you normally would in any other component
-  const [user, setUser] = useState(null);
+    // call getUserFromToken() to get our initial user state
+    const [user, setUser] = useState(getUserFromToken());
 
-  // This is the user state and the setUser function that will update it!
-  // This variable name isn't special; it's just convention to use `value`.
-  const value = { user, setUser };
+    const value = { user, setUser };
 
-  return (
-    <UserContext.Provider value={value}>
-      {/* The data we pass to the value prop above is now available to */}
-      {/* all the children of the UserProvider component. */}
-      {children}
-    </UserContext.Provider>
-  );
+    return (
+        <UserContext.Provider value={value}>
+            {children}
+        </UserContext.Provider>
+    );
 };
 
 // When components need to use the value of the user context, they will need
